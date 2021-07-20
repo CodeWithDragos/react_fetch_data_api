cdd07a7b06bcfea739564a6b500a92bcf8ee6094
Bogdan Nedelcu <bogdanned32@gmail.com>
2021-07-20 14:08:50 +0200

    create data with post

--- src/App.js
+++ src/App.js
@@ -2,14 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
 
+// REST API
+const apiUrl = "http://localhost:3004/posts"
+
 function App() {
   const [postList, setPostList] = useState([])
+  const [newPostTitle, setNewPostTitle] = useState("")
 
   // in the scope of the component
   async function fetchPostList(){
-    const response = await fetch("http://localhost:3004/posts")
+    const response = await fetch(apiUrl) // GET
     const data = await response.json()
     setPostList(data) // update our state
+  }
+
+  async function createArticle(articleData){
+    // create a post/article
+    const response = await fetch(apiUrl, {
+      method: "POST",
+      body: JSON.stringify(articleData),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    // refetch and refresh the UI
+    fetchPostList() 
   }
 
   useEffect(() => {
@@ -23,6 +40,11 @@ function App() {
           <p>{post.title}</p>
         </div>
       } )}
+      <input value={newPostTitle} onChange={(event) => (setNewPostTitle(event.target.value))}></input>
+      <button onClick={() => {
+        createArticle({"title": newPostTitle, "author": "user" })
+        setNewPostTitle("")// clears the input
+      }}>Create a fake post</button>
     </div>
   );
 }
